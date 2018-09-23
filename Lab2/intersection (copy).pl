8960718a74d0917aedfc73a12a6c31464693fdf9
Valerio Colitta <valco263@parlomba4.ida.liu.se>
2018-09-23 14:52:35 +0200

    add report

---
+++ Lab2/intersection (copy).pl
@@ -0,0 +1,20 @@
+%inter1(S1, S2, I) - Creates in I the intersection of the two sets S1 and S2
+inter1([], S2, []).
+inter1([H|T], S2, [H|TI]) :- memberl(H,S2), \+memberl(H,T), inter1(T,S2,TI).
+inter1([H|T], S2, TI) :- memberl(H,S2), memberl(H,T), inter1(T,S2,TI).
+inter1([H|T], S2, I) :- \+memberl(H,S2),inter1(T,S2,I).
+
+
+%memberl(X, L) - is True if X is a member of the list L
+memberl(X, [X|_]).
+memberl(X, [H|T]) :- memberl(X, T).
+
+%inter(S1,S2,I) - Performs the intersection of S1 and S2 and then sorts the resulting list and put it in I
+inter(S1,S2,I) :- inter1(S1,S2,K), sort(K,I).
+
+
+%inter(S1, S2, I) - Creates in I the intersection of the two non sets S1 and S2
+inter([], S2, []).
+inter([H|T], S2, [H|TI]) :- memberl(H,S2), \+memberl(H,T), inter(T,S2,TI).
+inter([H|T], S2, TI) :- memberl(H,S2), memberl(H,T), inter(T,S2,TI).
+inter([H|T], S2, I) :- \+memberl(H,S2),inter(T,S2,I).
