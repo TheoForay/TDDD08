63dd7bee401c945fbf046a98e6cf06e63295f3e5
Valerio Colitta <valco263@parlomba2.ida.liu.se>
2018-10-03 22:49:55 +0200

    add ex 3

--- Lab2/powset.pl
+++ Lab2/powset.pl
@@ -1,10 +1,10 @@
 %powset(S,L) - Returns a subset of S into L
-powset([], []).
-powset([H|T], [H|L]) :- powset(T,L).
 powset([H|T], L) :- powset(T,L).
+powset([H|T], [H|L]) :- powset(T,L).
+powset([], []).
 
 %powerset(S,R) - Returns all subsets of S into a R
-powerset(S, R) :- findall(X, powset(S,X), R).
+powerset(S, R) :- findall(X, powset(S,X), RR), sort(RR,R).
 
 %powerset([a,b,c], X).
-%X = [[a,b,c],[a,b],[a,c],[a],[b,c],[b],[c],[]]
+%X = [[],[a],[a,b],[a,b,c],[a,c],[b],[b,c],[c]] 
